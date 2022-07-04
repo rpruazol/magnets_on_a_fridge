8576db1c4b8158a0de27f1a6213f2a94c58b3006
rpruazol <rpruazol@gmail.com>
2022-07-04 02:43:32 -0400

    drag start and drag end event listeners

--- scripts/index.js
+++ scripts/index.js
@@ -10,22 +10,60 @@ const alphabet = {
 const root = document.getElementById("root")
 
 
-function render() {
-    for(let i = 0; i<=alphabet.captitalLetters.length; i++){
-        randomColor()
-        const letterElement = document.createElement('p')
-        letterElement.setAttribute('class', 'letter')
-        const letter = document.createTextNode(alphabet.captitalLetters[i]);
-        letterElement.append(letter)
-        // value="letter"
 
-        root.appendChild(letterElement)
+
+function generateLetter(letter){
+    const letterElement = document.createElement('p')
+    letterElement.setAttribute('class', 'letter')
+    letterElement.setAttribute('draggable', 'true')
+    const newLetter = document.createTextNode(letter)
+    letterElement.append(newLetter)
+    letterElement.style.color = randomColor()
+    addEventListeners(letterElement)
+    
+    return letterElement
+}
+
+function randomColor(){
+    return `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)})`
+}
+
+function randomNumber(num){
+    return Math.floor(Math.random() * num)
+}
+
+function render() {
+    for(let i = 0; i<alphabet.captitalLetters.length; i++){
+        const newLetter = generateLetter(alphabet.captitalLetters[i])
+        root.appendChild(newLetter)
     }
 }
 
+function addEventListeners(element) {
+    element.addEventListener('dragstart', onDragStart);
+	element.addEventListener('dragend', onDragEnd);
+}
 
-function randomColor(){
-    return
+function onDragStart(event) {
+    let target = event.target;
+	if (target && target.nodeName == 'P') {
+        // Store a ref. on the dragged elem
+	    const pSrc = target.textContent;
+	    // parkingSimulation.dragged = target;
+	    event.dropEffect = 'linkMove';
+	    event.dataTransfer.setData('text/uri-list', pSrc);
+	    event.dataTransfer.setData('text/plain', pSrc);
+
+	    // Make it half transparent
+	    event.target.style.opacity = .1;
+	}
+}
+
+function onDragEnd(event) {
+	if (event.target && event.target.nodeName == 'P') {
+	    // Reset the transparency
+	    event.target.style.opacity = '';
+	}
 }
 
 render()
